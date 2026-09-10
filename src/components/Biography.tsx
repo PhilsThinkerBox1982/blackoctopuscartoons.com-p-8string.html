f791ce3385db2465f3f94556f2f10fbb8a1d1fea
PhilsThinkerBox1982 <10136073+PhilsThinkerBox1982@users.noreply.github.com>
2026-09-09 17:49:51 -0700

    Update Biography.tsx

--- src/components/Biography.tsx
+++ src/components/Biography.tsx
@@ -302,7 +302,7 @@ export const Biography: React.FC<BiographyProps> = ({ profile, darkMode, onOpenR
               <p className={`text-xs mt-1.5 ${
                 darkMode ? 'text-slate-400' : 'text-slate-500'
               }`}>
-              Coursework focus : General Education 
+              Coursework focus : General Education
 {profile.education[0].focus}
               </p>
             </div>
